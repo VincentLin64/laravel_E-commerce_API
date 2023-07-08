246e2d58f24d0da3515f24ce01d608c1ef44da30
root <root@localhost.localdomain>
2023-07-08 10:39:10 +0800

    #add ReadData(NoDB)

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $data = $this->getData();
+        return response($data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    public function getData()
+    {
+        return [
+            ['title' => '測試商品1', 'content' => '棒', 'price' => 50],
+            ['title' => '測試商品2', 'content' => '讚', 'price' => 55],
+        ];
+    }
+}
