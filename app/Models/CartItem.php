aaf07c7562ca8a518279c4e18d58e5d1554fc1cb
root <root@localhost.localdomain>
2023-07-08 16:58:53 +0800

    #add Model CartItem

---
+++ app/Models/CartItem.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CartItem extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+//        'cart_id',
+//        'product_id',
+//        'quantity',
+    ];
+    protected $guarded = [
+        'quantity'
+    ];
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
+    protected $appends = [
+      'current_price'
+    ];
+
+    public function getCurrentPriceAttribute() {
+        return $this->quantity * 10;
+    }
+}
