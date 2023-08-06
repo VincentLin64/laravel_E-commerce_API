e357d1f582a771ba8aea1aa2d243e56b183bb89c
root <root@localhost.localdomain>
2023-08-06 17:08:09 +0800

    #add ErrorException

--- app/Http/Services/ShortUrlService.php
+++ app/Http/Services/ShortUrlService.php
@@ -15,19 +15,27 @@ class ShortUrlService
 
     public function makeShortUrl($url)
     {
-        $accessToken = env('SHORT_URL_ACCESS_TOKEN');
-        $data = ['url' => $url];
+        // try 外的錯誤
+//        $oContents['a']['123'];
+        try {
+            $accessToken = env('SHORT_URL_ACCESS_TOKEN');
+            $data = ['url' => $url];
 
-        $vResponse = $this->client->request(
-            'POST',
-            "https://api.pics.ee/v1/links/?access_token=$accessToken",
-            [
-                'headers' => ['Content-Type' => 'application/json'],
-                'body' => json_encode($data)
-            ]
-        );
-        $oContents = json_decode($vResponse->getBody()->getContents());
-        dd($oContents);
+            $vResponse = $this->client->request(
+                'POST',
+                "https://api.pics.ee/v1/links/?access_token=$accessToken",
+                [
+                    'headers' => ['Content-Type' => 'application/json'],
+                    'body' => json_encode($data)
+                ]
+            );
+            $oContents = json_decode($vResponse->getBody()->getContents());
+            // try 內的錯誤
+//            $oContents['a']['123'];
+        } catch (\Throwable $th) {
+            report($th);
+            return $url;
+        }
         return $oContents->data->picseeUrl;
     }
 }
