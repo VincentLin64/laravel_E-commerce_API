f630f1797b9a1b3f5f45a6ad6a695eed186486bf
root <root@localhost.localdomain>
2023-08-06 21:43:20 +0800

    #Laravel Excel OrderToReport

--- app/Exports/OrdersExport.php
+++ app/Exports/OrdersExport.php
@@ -5,20 +5,44 @@ namespace App\Exports;
 use App\Models\Order;
 use Illuminate\Support\Facades\Schema;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
-class OrdersExport implements FromCollection, WithHeadings
+class OrdersExport implements FromCollection, WithHeadings, WithColumnFormatting
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        return Order::all();
+        $orders = Order::with(['user', 'cart.cartItems.product'])->get();
+        $vOrders = $orders->map(function ($order){
+            return[
+                $order->id,
+                $order->user->name,
+                $order->is_shipped,
+                $order->cart->cartItems->sum(function ($cartItem){
+                    return $cartItem->product->price * $cartItem->quantity;
+                }),
+                Date::dateTimeToExcel($order->created_at)
+            ];
+        });
+        return $vOrders;
     }
     public function headings(): array
     {
-        return Schema::getColumnListing('orders');
+        return ['編號', '購買者', '是否運送', '總價', '建立時間'];
         // TODO: Implement headings() method.
+    }
+    public function columnFormats(): array
+    {
+        return [
+            'B' => NumberFormat::FORMAT_TEXT,
+            'D' => NumberFormat::FORMAT_NUMBER_COMMA_SEPARATED1,
+            'E' => NumberFormat::FORMAT_DATE_YYYYMMDD
+        ];
+        // TODO: Implement columnFormats() method.
     }
 }
