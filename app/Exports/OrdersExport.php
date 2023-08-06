ecef3984730e83d8ddff2d9aee95c33bde07fdef
root <root@localhost.localdomain>
2023-08-06 22:05:43 +0800

    #Add Sheet Style

--- app/Exports/OrdersExport.php
+++ app/Exports/OrdersExport.php
@@ -6,15 +6,18 @@ use App\Models\Order;
 use Illuminate\Support\Facades\Schema;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithColumnFormatting;
+use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Events\AfterSheet;
 use PhpOffice\PhpSpreadsheet\Shared\Date;
 use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 
-class OrdersExport implements FromCollection, WithHeadings, WithColumnFormatting
+class OrdersExport implements FromCollection, WithHeadings, WithColumnFormatting, WithEvents
 {
     /**
     * @return \Illuminate\Support\Collection
     */
+    public $dataCount;
     public function collection()
     {
         $orders = Order::with(['user', 'cart.cartItems.product'])->get();
@@ -29,6 +32,7 @@ class OrdersExport implements FromCollection, WithHeadings, WithColumnFormatting
                 Date::dateTimeToExcel($order->created_at)
             ];
         });
+        $this->dataCount = $orders->count();
         return $vOrders;
     }
     public function headings(): array
@@ -44,5 +48,39 @@ class OrdersExport implements FromCollection, WithHeadings, WithColumnFormatting
             'E' => NumberFormat::FORMAT_DATE_YYYYMMDD
         ];
         // TODO: Implement columnFormats() method.
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class => function (AfterSheet $event) {
+                $event->sheet->getDelegate()->getColumnDimension('A')->setWidth(50);
+                for ($i = 0; $i < $this->dataCount; $i++){
+                    $event->sheet->getDelegate()->getRowDimension($i)->setRowHeight(50);
+                }
+                $event->sheet->getDelegate()->getStyle('A1:B'.$this->dataCount)->getAlignment()->setVertical('center');
+                $event->sheet->getDelegate()->getStyle('A1:A' . $this->dataCount)->applyFromArray([
+                    'font' => [
+                        'name' => 'Arial',
+                        'bold' => true,
+                        'italic' => true,
+                        'color' => [
+                            'rgb' => 'FF0000'
+                        ]
+                    ],
+                    'fill' => [
+                        'fillType' => \PhpOffice\PhpSpreadsheet\Style\Fill::FILL_SOLID,
+                        'startColor' => [
+                            'rgb' => '000000'
+                        ],
+                        'endColor' => [
+                            'rgb' => '000000'
+                        ]
+                    ],
+                ]);
+                $event->sheet->getDelegate()->mergeCells('G1:H1');
+            }
+        ];
+        // TODO: Implement registerEvents() method.
     }
 }
