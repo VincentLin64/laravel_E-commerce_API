3a6c0a7b8fd4c40eeef7a063316f75779e2a7f0d
Vincent <vincent64e@gmail.com>
2024-01-21 17:16:04 +0800

    新增csrf token

--- resources/views/layout/admin_modal.blade.php
+++ resources/views/layout/admin_modal.blade.php
@@ -1,17 +1,22 @@
 <div class="modal fade" id="upload-image" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">上傳圖片</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form action="/admin/products/upload-image" method="post" enctype="multipart/form-data">
+            <form action="/admin/products/upload-image" method="post" enctype="multipart/form-data">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">上傳圖片</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
                     <input type="hidden" id="product_id" name="product_id">
-                    <input type="file" id="product_image" name="product_image">
-                    <input type="submit" value="送出">
-                </form>
-            </div>
+                    <label for="product_image" class="form-label"></label>
+                    <input id="product_image" class="form-control" type="file" name="product_image">
+                    @csrf
+
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">送出</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
@@ -19,16 +24,20 @@
 <div class="modal fade" id="import" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header">
-                <h1 class="modal-title fs-5" id="exampleModalLabel">匯入Excel</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <form action="/admin/products/excel/import" method="post" enctype="multipart/form-data">
-                    <input type="file" id="excel" name="excel">
-                    <input type="submit" value="送出">
-                </form>
-            </div>
+            <form action="/admin/products/excel/import" method="post" enctype="multipart/form-data">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">匯入Excel</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <label for="excel" class="form-label"></label>
+                    <input class="form-control" type="file" id="excel" name="excel">
+                    @csrf
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">送出</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,9 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+    @vite('resources/sass/app.scss')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://kit.fontawesome.com/4ed71e3587.js" crossorigin="anonymous"></script>
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>電商網站</title>
 </head>
 <body>
